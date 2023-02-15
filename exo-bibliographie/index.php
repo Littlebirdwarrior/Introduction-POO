2feb9512bdf5bb81ba92f439712b6de1816cf167
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-02-15 22:58:28 +0100

    Ajouts index exo2-biblio

---
+++ exo-bibliographie/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Introduction POO - Exo - Banque - Exo 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    </style>
+</head>
+
+<body>
+    <h1>Introduction POO - Exo - Banque - Exo 1</h1>
+    <p> <b>Consignes : </b><br>
+    Vous êtes chargé(e) de créer un projet orienté objet permettant de gérer des livres et leurs auteurs respectifs.
+    </p>
+    <h2>Affichage :</h2>
+
+    <?php
+    //importe mes classe
+    spl_autoload_register(function ($class_name) {
+        require $class_name . '.php';
+    });
+
+    
+
+
+    
+
+    ?>
+
+</body>
+
+</html>
