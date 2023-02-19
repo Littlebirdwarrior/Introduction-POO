f06f6da1c03ccdda138abe09fe9e4f018790fc53
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-02-19 17:08:42 +0100

    Affichage par genre débug, le pb venait du __toString()

--- exo-cinema/Film.php
+++ exo-cinema/Film.php
@@ -1,118 +1,157 @@
-<?php 
+<?php
 
 //Les films seront caractérisés par leur titre, leur date de sortie en France, 
 //leur  durée (en minutes) ainsi que leur réalisateur (unique, avec nom, prénom, sexe et date de naissance). 
 //Un résumé du film (synopsis)  pourra  éventuellement être  renseigné.  
 //Chaque  film  est  caractérisé  par  un  seul  genre cinématographique (science-fiction, aventure, action, ...).
 
-class Film {
+class Film
+{
     private string $_titre;
     private int $_dateSortie;
     private int $_duree;//à convertir en DateTime
     private Realisateur $_realisateur;
     private string $_resume;
     private Genre $_genre;
-    private array $castings;
 
-    public function __construct( string $titre, int $dateSortie, Realisateur $realisateur, int $duree, string $resume, Genre $genre){
+    private array $_castings;
+
+    public function __construct(string $titre, int $dateSortie, Realisateur $realisateur, int $duree, string $resume, Genre $genre)
+    {
         $this->_titre = $titre;
         $this->_dateSortie = $dateSortie;
         $this->_duree = $duree;
         $this->_realisateur = $realisateur;
         $this->_resume = $resume;
+        $this->_realisateur->addFilm($this);//cf class realisateur
         $this->_genre = $genre;
-        $this-> _genre -> addFilm($this);//cf class genre
-        $this-> _realisateur -> addFilm($this);//cf class realisateur
-        //$this -> _castings = [];
+        $this->_genre->addFilm($this);//cf class genre
+        $this->_castings = [];
     }
 
     //**Setter et Getter */
     //Titre
 
-    public function getTitre(){
+    public function getTitre()
+    {
         return $this->_titre;
     }
 
-    public function setTitre(string $titre){
+    public function setTitre(string $titre)
+    {
         $this->_titre = $titre;
     }
 
     //date
-    public function getDateSortie(){
+    public function getDateSortie(): int
+    {
         return $this->_dateSortie;
     }
 
-    public function setDateSortie( int $dateSortie){
+    public function setDateSortie(int $dateSortie)
+    {
         $this->_dateSortie = $dateSortie;
     }
 
     //duree
-    public function getDuree(){
-        return $this ->_duree;
+    public function getDuree()
+    {
+        return $this->_duree;
 
     }
 
-    public function setDuree(int $duree) {
+    public function setDuree(int $duree)
+    {
         $this->_duree = $duree;
     }
 
     //resume
-    public function getResume(){
-        return $this -> _resume;
+    public function getResume()
+    {
+        return $this->_resume;
     }
 
-    public function setResume(string $resume){
+    public function setResume(string $resume)
+    {
         $this->_resume = $resume;
     }
 
     //Realisateur
 
     //genre
-    public function getRealisateur(){
-        return $this -> _realisateur;
+    public function getRealisateur()
+    {
+        return $this->_realisateur;
     }
-    
-    public function setRealisateur( Realisateur $realisateur){
-        $this -> _realisateur = $realisateur;
+
+    public function setRealisateur(Realisateur $realisateur)
+    {
+        $this->_realisateur = $realisateur;
     }
 
     //genre
-    public function getGenre(){
-        return $this -> _genre;
+    public function getGenre()
+    {
+        return $this->_genre;
     }
 
-    public function setGenre( Genre $genre){
-        $this -> _genre = $genre;
+    public function setGenre(Genre $genre)
+    {
+        $this->_genre = $genre;
     }
 
     //**Méthode */
 
     //Convertir dateSortie en année
-    public function dateSortie(){
+    public function dateSortie()
+    {
         $getDate = $this->getDateSortie(); //je récupère ma date au format in
         $dateTimeStamp = mktime(0, 0, 0, 1, 1, $getDate);//je la convertir en Timestamp Unix, la fonction a besoin des arguments suivants "mktime($heure, $minute, $seconde, $mois, $jour, $annee, $est_dst)"
         $dateSortie = date('Y', $dateTimeStamp);//je crée ma date et la formate ma date en année
-        return "Année de sortie : ".$dateSortie;
+        return "Année de sortie : " . $dateSortie;
     }
 
     //Convertir la duree du format int en durée
 
-    public function getDureeTime(){
-        $getDate = $this ->getDuree();//je recupere ma durée en minutes
-        $convertSecond = $getDate *60;//je convertis mes minutes en seconde (le calcul des dates et durées se fait à partir des timestamp unix (en seconde))
+    public function getDureeTime()
+    {
+        $getDate = $this->getDuree();//je recupere ma durée en minutes
+        $convertSecond = $getDate * 60;//je convertis mes minutes en seconde (le calcul des dates et durées se fait à partir des timestamp unix (en seconde))
         $date = date('H:i', $convertSecond); // Creation et formatage de la date
-        return "Durée : ". $date;
+        return "Durée : " . $date;
+    }
+
+    //Le casting
+
+    public function addCasting($_casting)
+    {
+        $this->_castings[] = $_casting;
+    }
+
+    public function afficherCasting()
+    {
+        $result = "<h3> Casting </h3>";
+        if (count($this->_castings) == 0) {
+            // message pas de casting
+            $result .= "Pas de casting pour ce film !";
+        } else {
+            foreach ($this->_castings as $casting) {
+                $result .= $casting->get_role() . " est joué par " . $casting->get_acteur() . "<br>";
+            }
+        }
+        return $result;
     }
 
 
     //Affichage
-    public function __toString(){
-       return   "<b>" . $this ->getTitre() . "</b> 
-                <br> " . $this-> DateSortie() . 
-                "<br> " . $this -> getDureeTime() . 
-                " <br> Genre : ". $this -> getGenre() . 
-                " <br> Réalisateur : ". $this -> getRealisateur(). 
-                " <br> Synopsys : " . $this->getResume() . ".<hr>";
+    public function __toString()
+    {
+        return "<b>" . $this->getTitre() . "</b> 
+                <br> " . $this->DateSortie() .
+            "<br> " . $this->getDureeTime() .
+            " <br> Genre : " . $this->getGenre() .
+            " <br> Réalisateur : " . $this->getRealisateur() .
+            " <br> Synopsys : " . $this->getResume() . ".<hr>";
     }
 
 }
--- exo-cinema/Genre.php
+++ exo-cinema/Genre.php
@@ -1,48 +1,71 @@
-<?php 
+<?php
 
 //Les films seront caractérisés par leur titre, leur date de sortie en France, 
 //leur  durée (en minutes) ainsi que leur réalisateur (unique, avec nom, prénom, sexe et date de naissance). 
 //Un résumé du film (synopsis)  pourra  éventuellement être  renseigné.  
 //Chaque  film  est  caractérisé  par  un  seul  genre cinématographique (science-fiction, aventure, action, ...).
 
-class Genre {
+class Genre
+{
     private string $_genre;
-    private array $_films = [];
+    private array $_films;
 
-    public function __construct(string $genre) {
+    public function __construct(string $genre)
+    {
         $this->_genre = $genre;
         $this->_films = [];
     }
 
     //**Setter / Getter */
 
-    public function getGenre(){
+    // Genre
+
+    public function getGenre()
+    {
         return $this->_genre;
     }
-    public function setGenre(string $genre){
+
+    public function setGenre(string $genre)
+    {
         $this->_genre = $genre;
     }
+
+    function __toString(): string
+    {
+        return $this->_genre;
+    }
+
+    // Film
+
+    public function getFilms()
+    {
+        return $this->_films;
+    }
+
+    public function setFilms(array $films)
+    {
+        return $this->_films = $films;
+    }
+
 
 
     //** Methode*/
     //Ajouter des films à mon tableau (cf class livre)
 
-    public function addFilm(Film $film) {
+    public function addFilm(Film $film)
+    {
         $this->_films[] = $film;
     }
 
-    public function filmsParGenres(){
+    public function filmsParGenres()
+    {
         $films = $this->_films;
-        foreach ($films as $film){
-            $result = $film .'<br>';
+        foreach ($films as $film) {
+            echo $film;
         }
-        return $result;
-    }
-
-    public function __toString(){
-        return $this->_genre;
     }
 
 
 }
+
 ?>
